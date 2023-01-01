88b4e0acb3f3af9de6cee55928a0b4caca8a2e8b
nabigha-mogharbel <nabiga.ba.mo1998@gmail.com>
2023-01-01 21:08:02 +0200

    added create route, hash method works

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,6 +3,35 @@ const bcrypt=require('bcrypt')
 require('dotenv').config()
 const auth=require('../middleware/auth')
 module.exports = function(app, database){
+    app.post('/signup', async (req,res) => {
+        let isRegistered=false;
+        database.map(user => {
+            if(user.username===req.query.username){isRegistered=true}
+        })
+        if(isRegistered){
+            res.send('username is already used')
+        } else if (req.query.username && req.query.password) {
+            let newUser={
+                'username': req.query.username,
+                'password': await bcrypt.hash(req.query.password, parseInt(process.env.SALT))
+            }
+            const fs = require("fs");
+            let data = fs.readFileSync(process.env.DATABASEPATH);
+            let dataJSON=JSON.parse(data)
+            let index=dataJSON.users.length
+            dataJSON.users.push(newUser)
+            fs.writeFileSync(process.env.DATABASEPATH, JSON.stringify(dataJSON));
+            let token = jwt.sign({
+                username: newUser.username,
+                userID: index
+            }, process.env.TOKEN_SECRET ,{ expiresIn: '4h'})
+            res.cookie('token',token,{ maxAge: 2 * 60 * 60 * 1000, httpOnly: true });
+            res.send('account created')
+        } else {
+            res.send('you have to provide a username and a password')
+        }
+        
+    })
     app.post('/login', async (req,res) => {
        let i=-1;
        database.map( (user,index) => {
@@ -51,7 +80,7 @@ module.exports = function(app, database){
        let data = fs.readFileSync(process.env.DATABASEPATH);
        let dataJSON=JSON.parse(data)
         if(req.query.newPassword && req.query.newUsername){
-            dataJSON.users[res.locals.index].password=await bcrypt.hash(req.query.newPassword, process.env.SALT);
+            dataJSON.users[res.locals.index].password=await bcrypt.hash(req.query.newPassword, parseInt(process.env.SALT));
             dataJSON.users[res.locals.index].username= req.query.newUsername;
             res.send('updated your account');
         }
@@ -60,7 +89,7 @@ module.exports = function(app, database){
             res.send('updated your account');
         }
         else if(req.query.newPassword){
-            dataJSON.users[res.locals.index].password=await bcrypt.hash(req.query.newPassword, process.env.SALT);
+            dataJSON.users[res.locals.index].password=await bcrypt.hash(req.query.newPassword, parseInt(process.env.SALT));
             res.send('updated your account');
         }
         fs.writeFileSync(process.env.DATABASEPATH, JSON.stringify(dataJSON))
